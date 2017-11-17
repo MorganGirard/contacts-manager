dd827e3aa6c779a2ff1cf345778f3102517b5bf6
M. Girard Morgan <mgirard@insa-10227>
2017-11-17 10:20:22 +0100

    Rendre serialisable l'artefact informatique Contact

--- src/main/java/com/fredericboisguerin/insa/Contact.java
+++ src/main/java/com/fredericboisguerin/insa/Contact.java
@@ -1,9 +1,13 @@
 package com.fredericboisguerin.insa;
 
-public class Contact {
+import java.io.Serializable;
+
+public class Contact implements Serializable {
     private String name;
     private String email;
     private String phoneNumber;
+
+    public Contact() {}
 
     public Contact(String name, String email, String phoneNumber) {
         this.name = name;
@@ -15,13 +19,19 @@ public class Contact {
         return this.name;
     }
 
+    public void setName(String name) { this.name = name; }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) { this.email = email; }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 
     @Override
     public String toString() {
